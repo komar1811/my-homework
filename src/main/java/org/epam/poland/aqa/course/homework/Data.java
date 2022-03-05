d85f8db923b82486b47103ba7f7d210cf0e09af1
komar1811 <64264022+komar1811@users.noreply.github.com>
2022-03-05 15:08:30 +0100

    Delete src directory

--- src/main/java/org/epam/poland/aqa/course/homework/Data.java
+++
@@ -1,7 +0,0 @@
-package org.epam.poland.aqa.course.homework;
-
-public class Data {
-        public static final  String AMAZON_POSTAL_CODE = "99801";
-        public static final  String AMAZON_LOCATION = "Juneau";
-        public static final  String AMAZON_COUNTRY = "Canada";
-}
