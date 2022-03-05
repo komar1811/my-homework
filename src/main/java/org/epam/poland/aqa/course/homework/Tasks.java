b3385c5523a4a163c2b71dca33e0e70cb75cb97d
komar1811 <64264022+komar1811@users.noreply.github.com>
2022-03-05 14:40:15 +0100

    Update Tasks.java

--- src/main/java/org/epam/poland/aqa/course/homework/Tasks.java
+++ src/main/java/org/epam/poland/aqa/course/homework/Tasks.java
@@ -1,6 +1,6 @@
 package org.epam.poland.aqa.course.homework;
 
-import org.openqa.selenium.By;
+ort org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,14 +9,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.epam.poland.aqa.course.homework.Data.AMAZON_COUNTRY;
-import static org.epam.poland.aqa.course.homework.Data.AMAZON_POSTAL_CODE;
-
 public class Tasks {
 
     public static String Task11() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
 
         driver.get("https://www.amazon.com/");
 
@@ -25,28 +23,30 @@ public class Tasks {
 
         WebElement deliveryPostalCodeInfo = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"GLUXZipUpdateInput\"]")));
-//        WebElement deliveryPostalCodeInfo = driver.findElement(By.xpath("//*[@id=\"GLUXZipUpdateInput\"]"));
-        deliveryPostalCodeInfo.sendKeys(AMAZON_POSTAL_CODE);
+        deliveryPostalCodeInfo.sendKeys("99801");
 
         WebElement applyButton = driver.findElement(By.xpath("//*[@id=\"GLUXZipUpdate\"]/span/input"));
         applyButton.click();
 
 
         WebElement continueButton = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"GLUXConfirmClose\"]")));
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[5]/div/div/div[2]/span/span/input")));
         continueButton.click();
 
-
-        WebElement deliverText = driver.findElement(By.xpath("//*[@id=\"glow-ingress-line2\"]"));
+        WebElement deliverText = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),\"Juneau\")]")));
         String result = deliverText.getText();
+
         driver.close();
         driver.quit();
+
+
 
         return result;
     }
 
     public static boolean Task12() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.amazon.com/");
@@ -61,12 +61,17 @@ public class Tasks {
         WebElement deliveryCountry = new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),\"Poland\")]")));
 
-        return deliveryCountry.getText().contains("Poland");
+        boolean result = deliveryCountry.getText().contains("Poland");
+
+        driver.close();
+        driver.quit();
+
+        return result;
 
     }
 
     public static boolean Task13() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.amazon.com/");
@@ -79,24 +84,25 @@ public class Tasks {
         deliveryCountriesList.click();
 
         WebElement deliveryCountry = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),\""+AMAZON_COUNTRY+"\")]")));
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[6]/div/div/ul/li[47]/a")));
         deliveryCountry.click();
 
+
+
         WebElement doneButton = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(text(),\"Done\")]")));
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[3]/div/div/div[2]/span/span/span/button")));
         doneButton.click();
 
         WebElement goods = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"GaZP3LVmEhr8BoXOucfocw\"]/div[2]/div[1]/div[1]/a/div[1]/img")));
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div[1]/div/div[2]/div[1]/div[1]/a/div[1]/img")));
         goods.click();
 
-        WebElement good = driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img"));
+        WebElement good = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img"));
         good.click();
 
-        WebElement goodCountry = driver.findElement(By.xpath("//*[contains(@class,\"inner\")]//*[contains(text(),\""+AMAZON_COUNTRY+"\")]"));
+        WebElement goodCountry = driver.findElement(By.xpath("//*[contains(@class,\"inner\")]//*[contains(text(),\"Canada\")]"));
 
 
-        return goodCountry.getText().contains(AMAZON_COUNTRY);
-
+        return goodCountry.getText().contains("Canada");
     }
 }
